2d186a3995dd0c91d877f948cb0de22bbf86be22
dk460110 <dk460110@gmail.com>
2020-01-20 14:32:56 -0500

    Adding python script

--- cluster_cities_by_temp.py
+++ cluster_cities_by_temp.py
@@ -3,16 +3,20 @@
 
 # ### Import modules
 
-# In[1]:
+# In[ ]:
 
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from PIL import Image
 import requests
+from sklearn.cluster import KMeans
 
 
 # ### Get data from wikipedia
 
-# In[13]:
+# In[ ]:
 
 
 url = 'https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature'
@@ -22,7 +26,7 @@ data = pd.concat(pd.read_html(requests.get(url).content)).reset_index(drop = Tru
 
 # ### Clean and organize data
 
-# In[50]:
+# In[ ]:
 
 
 df = (data
@@ -33,13 +37,15 @@ df = (data
 
 df['Temp_F'] = df.Temp_String.str.split(pat = '(', expand = True)[1].str.split(pat = ')', expand = True)[0]
 df['City'] = df['City'] + ', ' + df['Country']
+df['Temp_F'] = pd.to_numeric(df['Temp_F'], errors = 'coerce')
 
 df = (df
           .filter(items = ['City', 'Temp_F'])
           .groupby(['City'])
           .agg(['min', 'max'])
           .reset_index()
-          .droplevel(0, axis = 1))
+          .droplevel(0, axis = 1)
+          .query('min == min and max == max'))
 
 df.columns = ['City', 'Min', 'Max']
 
@@ -47,5 +53,71 @@ df.columns = ['City', 'Min', 'Max']
 # In[ ]:
 
 
-print(df.sort_values('Min'))
+print(df)
+
+
+# ### Identify optimal number of clusters for K Means
+
+# In[ ]:
+
+
+temp_pairs = np.array(df[['Min', 'Max']])
+
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(temp_pairs)
+    wcss.append(kmeans.inertia_)
+    
+plt.plot(range(1,11), wcss)
+plt.title('Elbow WCSS')
+plt.xlabel('Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+
+# In[ ]:
+
+
+wcss_per = [(wcss[i] - wcss[i-1])/wcss[i-1] for i in range(1,10)]
+print(pd.Series(wcss_per))
+
+clusters = int(input('How many clusters should we use?: '))
+
+
+# ### Assign clusters
+
+# In[ ]:
+
+
+kmeans = KMeans(n_clusters = clusters, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+df['Cluster'] = kmeans.fit_predict(temp_pairs)
+
+
+# ### Plot
+
+# In[ ]:
+
+
+palette = ['red', 'blue', 'green', 'yellow', 'black', 'orange', 'purple']
+
+plt.figure(figsize = (20,10))
+
+for i in range(0, max(df['Cluster']) + 1):
+    plt.scatter(df[df['Cluster'] == i]['Min'], df[df['Cluster'] == i]['Max'], c = palette[i], s = 300)
+
+plt.title('Cities Clustered by Highest and Lowest Monthly Average Temperatures Fahrenheit')
+plt.xlabel('Lowest Monthly Average Temperature')
+plt.ylabel('Highest Monthly Average Temperature')
+plt.savefig('City_Temp_Clusters.png')
+
+
+# ### Open and view plot
+
+# In[ ]:
+
+
+img = Image.open('City_Temp_Clusters.png')
+img.show()
 
