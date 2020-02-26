78b2f976bac91e70b6724da3e78eafba87cf3f8c
dk460110 <dk460110@gmail.com>
2020-02-25 19:11:04 -0500

    Updating readme and gitignore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cities-by-temp
+
+Clone this repository and run cluster_cities_by_temp.py to scrape city temperature data from Wikipedia and cluster cities using KMeans clustering. Allows for selecting the number of clusters after viewing a WCSS plot. Also saves clustered data as as csv.
+
+Requires sklearn, pandas, numpy, matplotlib, PIL, and requests.
--- cluster_cities_by_temp.py
+++ cluster_cities_by_temp.py
@@ -2,10 +2,6 @@
 # coding: utf-8
 
 # ### Import modules
-
-# In[ ]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -15,20 +11,12 @@ from sklearn.cluster import KMeans
 
 
 # ### Get data from wikipedia
-
-# In[ ]:
-
-
 url = 'https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature'
 
 data = pd.concat(pd.read_html(requests.get(url).content)).reset_index(drop = True)
 
 
 # ### Clean and organize data
-
-# In[ ]:
-
-
 df = (data
           .melt(id_vars = ['Country', 'City'], value_vars = ['Jan', 'Feb', 'Mar', 'Apr',
                                                              'May', 'Jun', 'Jul', 'Aug',
@@ -49,18 +37,10 @@ df = (df
 
 df.columns = ['City', 'Min', 'Max']
 
-
-# In[ ]:
-
-
 print(df)
 
 
 # ### Identify optimal number of clusters for K Means
-
-# In[ ]:
-
-
 temp_pairs = np.array(df[['Min', 'Max']])
 
 wcss = []
@@ -76,10 +56,6 @@ plt.xlabel('Clusters')
 plt.ylabel('WCSS')
 plt.show()
 
-
-# In[ ]:
-
-
 wcss_per = [(wcss[i] - wcss[i-1])/wcss[i-1] for i in range(1,10)]
 print(pd.Series(wcss_per))
 
@@ -87,25 +63,19 @@ clusters = int(input('How many clusters should we use?: '))
 
 
 # ### Assign clusters
-
-# In[ ]:
-
-
 kmeans = KMeans(n_clusters = clusters, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
 df['Cluster'] = kmeans.fit_predict(temp_pairs)
 
 
 # ### Plot
-
-# In[ ]:
-
-
-palette = ['red', 'blue', 'green', 'yellow', 'black', 'orange', 'purple']
+# palette = ['red', 'blue', 'green', 'yellow', 'black', 'orange', 'purple']
 
 plt.figure(figsize = (20,10))
 
+plt.style.use('seaborn')
+
 for i in range(0, max(df['Cluster']) + 1):
-    plt.scatter(df[df['Cluster'] == i]['Min'], df[df['Cluster'] == i]['Max'], c = palette[i], s = 300)
+    plt.scatter(df[df['Cluster'] == i]['Min'], df[df['Cluster'] == i]['Max'], s = 300)
 
 plt.title('Cities Clustered by Highest and Lowest Monthly Average Temperatures Fahrenheit')
 plt.xlabel('Lowest Monthly Average Temperature')
@@ -114,12 +84,7 @@ plt.savefig('City_Temp_Clusters.png')
 
 
 # ### Open and view plot
-
-# In[ ]:
-
-
 img = Image.open('City_Temp_Clusters.png')
 img.show()
 
 df.to_csv('Cities_with_Clusters.csv')
-
